9801b3864728b918553bc75bcf70ed8e0a336384
Camiloriwi <camilomanco2005@gmail.com>
2024-04-23 11:52:31 -0500

    f

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
+import { TransferModule } from './transfer/transfer.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PersistenceModule } from './persistence/persistence/persistence.module';
+import dbConfig from './persistence/persistence/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+    TransferModule,
+  ],
   controllers: [],
   providers: [],
 })
